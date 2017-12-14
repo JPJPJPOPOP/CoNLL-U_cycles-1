8d3442e4dfbda7cfab019452900e0035a15e3b59
Cole Kissane <coler706@gmail.com>
2017-12-13 20:02:52 -0800

    Fix Some Errors

--- graph.py
+++ graph.py
@@ -10,25 +10,52 @@ class Graph():
 
     def add_edge(self, u, v):
         self.graph[u].append(v)
-
-    def is_cyclic_util(self, v, visited, rec_stack):
-        visited[v] = True
-        rec_stack[v] = True
-
+    #internal
+    def is_cyclic_util(self, v, visits_p,n):
+        visits=list(visits_p)
+        visits[v] +=1
+        bads=[]
+        if(visits[v]>1):
+            if(not(v==n)):
+                visits[v] +=-1
+                return []
+            return [[v]]
+            bads.append(v)
         for neighbor in self.graph[v]:
-            if not visited[neighbor]:
-                if self.is_cyclic_util(neighbor, visited, rec_stack):
-                    return True
-            elif rec_stack[neighbor]:
-                return True
-        rec_stack[v] = False
-        return False
+            cyclic_data=self.is_cyclic_util(neighbor, visits,n)
+            if len(cyclic_data)>0:
+                for bad in cyclic_data:
+                    if(v in bad):
+                        bad.remove(v)
+                    b=[v]+bad
+                    if(not(b in bads)):
+                        bads.append(b)
 
-    def is_cyclic(self):
-        visited = [False] * self.V
-        rec_stack = [False] * self.V
+        return bads
+    
+    def normalize_cycle(self,a):
+        b=list(a)
+        b.sort()
+        loc=a.index(b[0])
+        g=[0]*len(a)
+        for x in range(0,len(a)):
+            g[x-loc]=a[x]
+        return g
+    #returns existing cycles in the form of lists of node ids which are in a cycle
+    def cycle_list(self):
+        cycles=[]
+        visits= [0] * self.V
         for node in range(self.V):
-            if not visited[node]:
-                if self.is_cyclic_util(node, visited, rec_stack):
-                    return True
-        return False
+            if visits[node]<1:
+                rec= [0] * self.V
+                c_datas=self.is_cyclic_util(node, rec,node)
+                for c_data in c_datas:
+                    if(len(c_data)>0):
+                        c_data=self.normalize_cycle(c_data)
+                        if(not(c_data in cycles)):
+                            cycles.append(c_data)
+        
+        return cycles
+    #checks if graph has cycles
+    def is_cyclic(self):  
+        return len(self.cycle_list())>0
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # main.py
 import sys
-from conllu.parser import parse
+from parser import parse
 from graph import Graph
 
 if len(sys.argv) != 2:
-    raise Exception('Invalid Input')
+    raise Exception('Invalid Input, must be given a file which is in the conllu format')
+
 data = parse(open(sys.argv[1], 'r').read())
 print(data)
 g = Graph(len(data[0])+1)
@@ -13,7 +14,11 @@ g = Graph(len(data[0])+1)
 for word in data[0]:
     print(word['head'], " ", word['id'])
     g.add_edge(word['head'], word['id'])
+
+print("Has Cycles:")
 print(g.is_cyclic())
+print("Cycle List:")
+print(g.cycle_list())
 #     if (word['head'] in head_to_tail):
 #         head_to_tail[word['head']].append(word['id'])
 #     else:
